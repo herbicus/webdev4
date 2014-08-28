e108737d119bda19976b083b6b5f93c8ea5dd1f1
herbicus <sonofman_x@hotmail.com>
2014-08-27 22:39:03 -0400

    updated cornerstone frameworks

--- cornerstone/assets/js/main.js
+++ cornerstone/assets/js/main.js
@@ -1,1 +1,26 @@
 console.log("main.js loaded");
+
+if (!window.console) window.console = {};
+if (!window.console.log) window.console.log = function() {};
+
+var cornerstoneAPI = (function(options) {
+	var shared = {},
+		options = options || {}
+	
+	// EVENT HANDLERS
+
+
+	// BOTTOM BEFORE CLOUSURE
+	var init = function() {
+		console.log('cornerstone API works');
+		// myFunctions();
+	};
+	shared.init = init;
+
+	return shared;
+}());
+
+$(document).ready(function() {
+	cornerstoneAPI.init();
+
+});
